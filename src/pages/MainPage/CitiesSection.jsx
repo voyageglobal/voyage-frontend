7bb013d4d573175c48ef1452bac6bff98665a8c5
Vlad Mininzon <mininzon.v@gmail.com>
2024-05-27 22:07:16 +0300

    The Popular Cities section was created and added normalize.css

--- src/pages/MainPage/CitiesSection.jsx
+++ src/pages/MainPage/CitiesSection.jsx
@@ -5,17 +5,48 @@ import CityCard from '../../components/common/CityCard/CityCard';
 const CitiesSection = () => {
   return (
     <section className="cities">
-      <div className="cities-wrapper">
-        <CityCard />
-        <CityCard />
-        <CityCard />
-        <CityCard />
-        <CityCard />
-        <CityCard />
-        <CityCard />
-        <CityCard />
-        <CityCard />
-        <CityCard />
+      <div className="cities-container">
+        <h2 className="cities-title">Popular Cities</h2>
+        <div className="cities-info">
+          <h3 className="cities-subtitle">
+            Explore city guides for your next adventure
+          </h3>
+          <a href="#" className="cities-link-viewAll">
+            View all
+          </a>
+        </div>
+        <ul className="cities-cards-wrapper">
+          <li>
+            <CityCard />
+          </li>
+          <li>
+            <CityCard />
+          </li>
+          <li>
+            <CityCard />
+          </li>
+          <li>
+            <CityCard />
+          </li>
+          <li>
+            <CityCard />
+          </li>
+          <li>
+            <CityCard />
+          </li>
+          <li>
+            <CityCard />
+          </li>
+          <li>
+            <CityCard />
+          </li>
+          <li>
+            <CityCard />
+          </li>
+          <li>
+            <CityCard />
+          </li>
+        </ul>
       </div>
     </section>
   );
