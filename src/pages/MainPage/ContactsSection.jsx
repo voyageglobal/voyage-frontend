7b886ef6e3a395c628f1516f091ac407f2ed2fd4
Vlad Mininzon <mininzon.v@gmail.com>
2024-06-05 19:54:02 +0300

    vite-plugin-svgr was implemented + was added classNames to tags + was fixed styles for nearest sections

--- src/pages/MainPage/ContactsSection.jsx
+++ src/pages/MainPage/ContactsSection.jsx
@@ -1,68 +1,68 @@
 import './contactsSection.scss';
-import mailIcon from '../../assets/img/mailIcon.svg';
-import FacebookIcon from '../../components/common/icons/Facebook';
-import instaIcon from '../../assets/img/instaIcon.svg';
-import twitterIcon from '../../assets/img/twitterIcon.svg';
-import pinterestIcon from '../../assets/img/pinterestIcon.svg';
-import youtubeIcon from '../../assets/img/youtubeIcon.svg';
+import MailIcon from '../../assets/img/mailIcon.svg';
+import FacebookIcon from '../../assets/img/facebookIcon.svg';
+import InstaIcon from '../../assets/img/instaIcon.svg';
+import TwitterIcon from '../../assets/img/twitterIcon.svg';
+import PinterestIcon from '../../assets/img/pinterestIcon.svg';
+import YoutubeIcon from '../../assets/img/youtubeIcon.svg';
 
 const ContactsSection = () => {
   return (
     <section className="contacts">
-      <div className="contacts-wrapper">
-        <h2 className="contacts-title">CONTACT US </h2>
-        <p className="contacts-text">
+      <div className="contacts__wrapper">
+        <h2 className="contacts__title">CONTACT US </h2>
+        <p className="contacts__text">
           We are always happy to hear from our users! If you have any questions,
           suggestions, or just want to share your thoughts about our website,
           feel free to contact us. We are ready to answer all your inquiries.
         </p>
-        <div className="contacts-container">
-          <div className="contacts-info">
-            <h3 className="contacts-subtitle">Get in touch with us!</h3>
-            <div className="contacts-info-mail">
-              <img src={mailIcon} alt="The icon of letter" />
-              <p className="contacts-info-title">EMAIL</p>
-              <a href="#" className="contacts-info-email">
+        <div className="contacts__container">
+          <div className="contacts__info">
+            <h3 className="contacts__subtitle">Get in touch with us!</h3>
+            <div className="contacts__info-mail">
+              <MailIcon />
+              <p className="contacts__info-title">EMAIL</p>
+              <a href="#" className="contacts__info-email">
                 voyage@gmail.com
               </a>
             </div>
-            <div className="contacts-info-links">
-              <h3 className="contacts-subtitle">Connect with us</h3>
-              <ul className="contacts-info-list">
-                <li className="contacts-info-list-item">
+            <div className="contacts__info-links">
+              <h3 className="contacts__subtitle">Connect with us</h3>
+              <ul className="contacts__info-list">
+                <li className="contacts__info-list-item">
                   <a href="#">
                     <FacebookIcon />
                   </a>
                 </li>
-                <li className="contacts-info-list-item">
+                <li className="contacts__info-list-item">
                   <a href="#">
-                    <img src={instaIcon} alt="Instagram's icon" />
+                    <InstaIcon />
                   </a>
                 </li>
-                <li className="contacts-info-list-item">
+                <li className="contacts__info-list-item">
                   <a href="#">
-                    <img src={twitterIcon} alt="Twitter's icon" />
+                    <TwitterIcon />
                   </a>
                 </li>
-                <li className="contacts-info-list-item">
+                <li className="contacts__info-list-item">
                   <a href="#">
-                    <img src={pinterestIcon} alt="Pinterest's icon" />
+                    <PinterestIcon />
                   </a>
                 </li>
-                <li className="contacts-info-list-item">
+                <li className="contacts__info-list-item">
                   <a href="#">
-                    <img src={youtubeIcon} alt="Youtube's icon" />
+                    <YoutubeIcon />
                   </a>
                 </li>
               </ul>
             </div>
           </div>
-          <div className="contacts-form">
-            <h3 className="contacts-subtitle contacts-subtitle-form">
+          <div className="contacts__form">
+            <h3 className="contacts__subtitle contacts__subtitle--form">
               If you got any questions <br /> Please do not hesitate to send us
               a message.
             </h3>
-            <form>
+            <form className="contacts__form-container">
               <input type="text" name="name" placeholder="Your name" required />
               <input
                 type="email"
@@ -76,7 +76,9 @@ const ContactsSection = () => {
                 placeholder="Your message"
                 required
               ></textarea>
-              <button type="submit">Send Message</button>
+              <button className="contacts__form-btn" type="submit">
+                Send Message
+              </button>
             </form>
           </div>
         </div>
