a7d62a1de227387800d319664aaa90e56073de5d
Vlad Mininzon <mininzon.v@gmail.com>
2024-06-18 21:37:27 +0300

    OurGuidesSection was edited with tailwind + added text-shadow-plugin

--- src/pages/MainPage/OurGuidesSection.jsx
+++ src/pages/MainPage/OurGuidesSection.jsx
@@ -1,19 +1,22 @@
 import { memo } from 'react';
-import './ourGuidesSection.scss';
+import BackgroundOurGuides from '../../assets/img/mainPage/map.png';
 
 const OurGuidesSection = ({ guidesCount = 53, countriesCount = 21 }) => {
   return (
-    <section className="our-guides-section">
-      <div className="our-guides-section__wrapper">
-        <div className="our-guides-section__content">
-          <h2 className="our-guides-section__title">OUR GUIDES</h2>
-          <p className="our-guides-section__text">
+    <section className="min-h-[35.19rem] pb-28 pt-24">
+      <div
+        className="container mx-auto h-[35.19rem] w-[73rem] rounded-10px bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${BackgroundOurGuides})` }}
+      >
+        <div className="flex h-[29.88rem] w-96 flex-col justify-between pl-20 pt-40">
+          <h2 className="text-shadow-header font-third text-5xl">OUR GUIDES</h2>
+          <p className="font-fourth text-xl/6">
             Today there are <b>{guidesCount}</b> guides in <br />
             <b>{countriesCount}</b> countries in our community! <br /> Discover
             new cities and countries, or share your guide right now!
           </p>
           <button
-            className="our-guides-section__button"
+            className="bg-orange-color h-12 w-52 rounded-10px text-center font-fourth text-lg text-white"
             aria-label="A button leading to a page with all the guides."
           >
             BON VOYAGE
