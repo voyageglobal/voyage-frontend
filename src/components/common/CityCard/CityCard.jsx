d990193d0794ffbc39bff70197669a7934af7fb1
Vlad Mininzon <mininzon.v@gmail.com>
2024-06-18 19:04:07 +0300

    Tailwind text-xl was added

--- src/components/common/CityCard/CityCard.jsx
+++ src/components/common/CityCard/CityCard.jsx
@@ -2,7 +2,7 @@ const CityCard = ({ cityName, countryName, image }) => {
   return (
     <div className="rounded-10px flex h-80 w-44 flex-col bg-white/30 font-primary shadow-md">
       <img src={image} alt={`The picture of ${cityName}`} />
-      <p className="ml-1.5 mt-2.5 text-[1.25rem] uppercase">{cityName}</p>
+      <p className="ml-1.5 mt-2.5 text-xl uppercase">{cityName}</p>
       <p className="ml-1.5 mt-2.5 text-base">{countryName}</p>
     </div>
   );
