d4b12dce5775833e42659109c95921d41b6495b5
Vlad Mininzon <mininzon.v@gmail.com>
2024-12-27 11:16:59 +0200

    typo

--- src/data/transformCityItem.js
+++ src/data/transformCityItem.js
@@ -6,7 +6,7 @@ const transformCityItem = city => {
     name: city.name,
     description:
       city.description ||
-      'We don’t have a guide for this destination yet, but who needs one when adventure awaits? Pack your bags, bring your curiosity, and become the pioneer explorer of this hidden gem. Don’t forget to share your discoveries—your tips might just write the next guide!',
+      'We don’t have a description for this destination yet, but who needs one when adventure awaits? Pack your bags, bring your curiosity, and become the pioneer explorer of this hidden gem. Don’t forget to share your discoveries—your tips might just write the next guide!',
     country: {
       id: city.country?.id || null,
       name: city.country?.name,
