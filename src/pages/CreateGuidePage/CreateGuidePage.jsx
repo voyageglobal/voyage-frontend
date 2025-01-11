8cf2b925e74a79231f35f718818be9eee0a434a1
Vlad Mininzon <mininzon.v@gmail.com>
2025-01-11 18:05:43 +0200

    CreateGuidePage was created

--- src/pages/CreateGuidePage/CreateGuidePage.jsx
+++ src/pages/CreateGuidePage/CreateGuidePage.jsx
@@ -1,11 +1,14 @@
 import Page from '../../components/common/Page/Page';
+import CreateGuideHeroSection from './CreateGuideHeroSection';
 
 const CreateGuidePage = () => {
   return (
     <Page
       title="Create Your Travel Guide"
       description="Welcome to the guide creation page on Voyage. Share your travel experiences and tips by creating your own comprehensive travel guide. Help fellow travelers discover new places and hidden gems. Start crafting your unique journey now."
-    ></Page>
+    >
+      <CreateGuideHeroSection />
+    </Page>
   );
 };
 
