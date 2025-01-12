dd281d64d347488db6bfdac12600fe72c7cc07fa
Vlad Mininzon <mininzon.v@gmail.com>
2025-01-12 21:32:01 +0200

    CreateGuideFormSection was added

--- src/pages/CreateGuidePage/CreateGuidePage.jsx
+++ src/pages/CreateGuidePage/CreateGuidePage.jsx
@@ -1,5 +1,6 @@
 import Page from '../../components/common/Page/Page';
 import CreateGuideHeroSection from './CreateGuideHeroSection';
+import CreateGuideFormSection from './CreateGuideFormSection';
 
 const CreateGuidePage = () => {
   return (
@@ -8,6 +9,7 @@ const CreateGuidePage = () => {
       description="Welcome to the guide creation page on Voyage. Share your travel experiences and tips by creating your own comprehensive travel guide. Help fellow travelers discover new places and hidden gems. Start crafting your unique journey now."
     >
       <CreateGuideHeroSection />
+      <CreateGuideFormSection />
     </Page>
   );
 };
