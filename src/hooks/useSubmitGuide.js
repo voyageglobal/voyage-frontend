55ed8088c6baffaedf5e1381dfdd9b843d5810e5
Vlad Mininzon <mininzon.v@gmail.com>
2025-01-26 22:26:52 +0200

    added reset for form success

--- src/hooks/useSubmitGuide.js
+++ src/hooks/useSubmitGuide.js
@@ -1,21 +1,42 @@
 import { useMutation } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
 import instance from '../utilities/axios/axios';
 
 const submitGuide = async formData => {
-  const response = await instance.post('/guides', formData);
-  return response.data;
+  try {
+    const response = await instance.post('/guides', formData);
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || 'An unexpected error occurred.',
+    );
+  }
 };
 
-const useSubmitGuide = () => {
+const toastConfig = {
+  position: 'top-center',
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: false,
+  pauseOnHover: true,
+  draggable: true,
+};
+
+const useSubmitGuide = ({ resetForm }) => {
   return useMutation({
     mutationFn: submitGuide,
     onSuccess: data => {
       console.log('Guide submitted successfully:', data);
-      alert('Guide submitted successfully!');
+      toast.success('Guide submitted successfully!', toastConfig);
+      if (resetForm) resetForm();
     },
     onError: error => {
       console.error('Error submitting guide:', error);
-      alert('Failed to submit the guide. Please try again.');
+      toast.error(
+        'Failed to submit. You can continue editing and try submit again.',
+        toastConfig,
+      );
+      if (resetForm) resetForm();
     },
   });
 };
